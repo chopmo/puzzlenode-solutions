b751dea82b3fae9e904f82fbe377c5b567ebd81d
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2011-12-02 23:43:16 +0100

    create Puzzle class and make solver.rb just invoke that.

--- 14_six_degrees/solver.rb
+++ 14_six_degrees/solver.rb
@@ -1,20 +1,3 @@
 $:.unshift('lib')
-
-require 'user_repository'
-require 'input_parser'
-require 'connections'
-
-repo = UserRepository.new(InputParser.new(open(ARGV[0]).read).parse.values)
-
-repo.users.each do |user|
-  puts user.name
-  level = 1
-  while true
-    conns = Connections.new(repo, user.name).on_level(level)
-    break if conns.empty?
-    puts conns.sort.join(", ")
-    level += 1
-  end
-  puts # Note: Trailing newline had to be removed before submitting
-end
-
+require 'puzzle'
+puts Puzzle.new(open(ARGV[0]).read).solve
