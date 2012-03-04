63df401c6cfb7f88bd654b258f0f9879f51ecc76
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-03-04 15:26:15 +0100

    can now add numbers

--- 15_poor_mans_forth/lib/stacker.rb
+++ 15_poor_mans_forth/lib/stacker.rb
@@ -7,7 +7,24 @@ module Stacker
     end
 
     def execute(cmd)
-      stack << cmd 
+      case cmd
+      when /ADD/ then add
+      else 
+        # Assume it's an integer
+        push(cmd.to_i)
+      end
+    end
+
+    def add
+      push(pop + pop)
+    end
+
+    def push(e)
+      stack.unshift(e)
+    end
+
+    def pop
+      stack.shift
     end
   end
 end
