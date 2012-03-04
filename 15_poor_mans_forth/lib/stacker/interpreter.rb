d7a647bfb4db232a1808bdff50f303bcb783fe15
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-03-04 16:24:46 +0100

    refactoring

---
+++ 15_poor_mans_forth/lib/stacker/interpreter.rb
@@ -0,0 +1,72 @@
+module Stacker
+
+  class Interpreter
+    attr_reader :stack
+
+    def initialize
+      @stack = []
+    end
+
+    def command_map
+    end
+
+    def execute(cmd)
+      if %(ADD SUBTRACT MULTIPLY DIVIDE MOD < >).include?(cmd)
+        send(cmd.downcase)
+      elsif cmd == "="
+        equals
+      else
+        push(cmd.to_i)
+      end
+      # puts "After #{cmd}, stack is #{stack}"
+    end
+
+    def add
+      push(pop + pop)
+    end
+
+    def subtract
+      b, a = pop, pop
+      push(a - b)
+    end
+
+    def multiply
+      push(pop * pop)
+    end
+
+    def divide
+      b, a = pop, pop
+      push(a / b)
+    end
+
+    def mod
+      b, a = pop, pop
+      push a % b
+    end
+
+    def <
+      b, a = pop, pop
+      push a < b ? :true : :false
+    end
+
+    def >
+      b, a = pop, pop
+      push a > b ? :true : :false
+    end
+
+    def equals
+      push pop == pop ? :true : :false
+    end
+
+    private 
+
+    # XXX: use delegation
+    def push(e)
+      stack.push(e)
+    end
+
+    def pop
+      stack.pop
+    end
+  end
+end
