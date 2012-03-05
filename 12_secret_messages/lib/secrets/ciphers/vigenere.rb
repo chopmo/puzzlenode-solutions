26a44fc9c2e32093f4eb872adb8e98bc7f76aef1
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-03-05 20:14:28 +0100

    Implemented Vigenere cipher, decrypt only

--- 12_secret_messages/lib/secrets/ciphers/vigenere.rb
+++ 12_secret_messages/lib/secrets/ciphers/vigenere.rb
@@ -2,7 +2,8 @@ module Secrets
   module Ciphers
     class Vigenere < Cipher
       def initialize(key)
-        @key = key
+        @key = key.strip
+        @key_index = 0
       end
 
       def decrypt(enc)
@@ -10,9 +11,27 @@ module Secrets
       end
       
       def decrypt_char(c)
-        c
+        return c unless letters.include?(c)
+        decrypted = shift_char(c, -current_shift)
+        inc_key_index
+        decrypted
       end
 
+      def inc_key_index
+        @key_index = (@key_index + 1) % @key.size
+      end
+
+      def current_shift
+        @key[@key_index].ord - letters.first.ord
+      end
+
+      private
+      def shift_char(c, amount)
+        char_idx = letters.index(c)
+        return c unless char_idx
+        shifted_idx = (char_idx + amount + letters.size) % letters.size
+        letters[shifted_idx]
+      end
     end
   end
 end
