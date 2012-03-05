9bc7320df5486496349917563fcf0cab1e9836c8
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-03-05 22:35:12 +0100

    input interpreter a bit clunky but working

---
+++ 09_turtle_tracks/lib/turtles/interpreter.rb
@@ -0,0 +1,53 @@
+module Turtles
+  class Interpreter
+    def initialize(lines)
+      @lines = lines
+      @index = -1
+      parse_input
+    end
+
+    def read_command
+      return nil if @index >= @commands.size
+      @index += 1
+      @commands[@index]
+    end
+
+    private 
+
+    def parse_input
+      @commands = []
+      @lines.map { |l|
+        parse_line(l)
+      }
+    end
+
+    def parse_line(l)
+      if l.index("REPEAT") == 0
+        parse_repetition(l)
+      else
+        parse_simple_command(l)
+      end
+    end
+
+    def parse_repetition(l)
+      tokens = l.split
+
+      tokens.shift # discard REPEAT
+      count = tokens.shift.to_i
+      tokens.shift # discard [
+      tokens.pop   # discard ]
+
+      count.times do
+        idx = 0
+        while idx < tokens.size
+          parse_simple_command("#{tokens[idx]} #{tokens[idx+1]}")
+          idx += 2
+        end
+      end
+    end
+
+    def parse_simple_command(cmd)
+      @commands << cmd
+    end
+  end
+end
--- 09_turtle_tracks/lib/turtles/solver.rb
+++ 09_turtle_tracks/lib/turtles/solver.rb
@@ -1,19 +1,29 @@
 require 'turtles/canvas'
+require 'turtles/interpreter'
+
 module Turtles
   class Solver
     def initialize(filename)
       @input_lines = File.readlines(filename)
+      @interpreter = Interpreter.new(command_lines)
     end
 
     def canvas_size
       @input_lines.first.to_i
     end
 
+    def command_lines
+      @input_lines.drop(2)
+    end
 
     def run
       @canvas = Canvas.new(canvas_size)
-      puts "Resulting canvas:"
-      puts @canvas.to_s
+      # puts "Resulting canvas:"
+      # puts @canvas.to_s
+
+      while cmd = @interpreter.read_command
+        puts cmd
+      end
     end
   end
 end
