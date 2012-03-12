d6b6992273a05734350f484cacd29f1f10cd79aa
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-03-12 20:37:05 +0100

    fix component parser spec

--- 10_all_wired_up/spec/component_parser_spec.rb
+++ 10_all_wired_up/spec/component_parser_spec.rb
@@ -1,16 +1,27 @@
 require 'wires/component_parser' 
+require 'wires/cell' 
 
 module Wires
   describe ComponentParser do
-    describe ComponentBuilder do
-      let (:diagram) { Diagram.new("@01-|AOXN")
-        it "creates a lightbulb from a @ cell" do
-          
-          b = ComponentBuilder.new()
-          b.at([0,0]).should be_a Lightbulb
-        end
-      end
 
+    def parse(char) 
+      ComponentParser.parse(Cell.new(nil, 0, 0, char))
+    end
+    
+    it "creates a lightbulb from a @ cell" do
+      parse("@").should be_a Lightbulb
+    end
+
+    it "creates a signal from 1" do
+      c = parse("1")
+      c.should be_a Signal
+      c.value.should be_true
+    end
+
+    it "creates a signal from 0" do
+      c = parse("0")
+      c.should be_a Signal
+      c.value.should be_false
     end
   end
 end
