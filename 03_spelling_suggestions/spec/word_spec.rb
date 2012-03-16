fbb28ae393435d86d501187b94d108707ec1ddc8
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-03-16 20:01:06 +0100

    it handles when one word is a prefix of the other

--- 03_spelling_suggestions/spec/word_spec.rb
+++ 03_spelling_suggestions/spec/word_spec.rb
@@ -7,26 +7,32 @@ describe Word do
   end
 
   describe "#lcss" do
-    describe "trivial cases" do
-      it "handles empty words" do
-        lcss("", "").should == ""
-      end
+    it "handles empty words" do
+      lcss("", "").should == ""
+    end
 
-      it "handles when first word is empty" do
-        lcss("", "foo").should == ""
-      end
-      
-      it "handles when second word is empty" do
-        lcss("foo", "").should == ""
-      end
+    it "handles when first word is empty" do
+      lcss("", "foo").should == ""
+    end
+    
+    it "handles when second word is empty" do
+      lcss("foo", "").should == ""
+    end
 
-      it "handles identical single-letter words" do
-        lcss("x", "x").should == "x"
-      end
+    it "handles identical single-letter words" do
+      lcss("x", "x").should == "x"
+    end
 
-      it "handles longer identical words" do
-        lcss("foo", "foo").should == "foo"
-      end
+    it "handles longer identical words" do
+      lcss("foo", "foo").should == "foo"
+    end
+
+    it "handles when w1 is a prefix of w2" do
+      lcss("foo", "foobar").should == "foo"
+    end
+    
+    it "handles when w2 is a prefix of w1" do
+      lcss("foobar", "foo").should == "foo"
     end
   end
 end
