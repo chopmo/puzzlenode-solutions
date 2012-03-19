643179a8974514b0c3641234fa5babe353898bee
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-03-19 23:58:00 +0100

    working and fast solution. Added a few notes

---
+++ 03_spelling_suggestions/README.md
@@ -0,0 +1,6 @@
+Note that the class name Word is a bit misleading - I originally
+thought that I was going to model this the OO ways as I tend to do,
+but (obviously I guess) that was WAY too slow.
+
+No OO going on here, just a couple of simple methods in the Word
+class.
--- 03_spelling_suggestions/lib/word.rb
+++ 03_spelling_suggestions/lib/word.rb
@@ -1,49 +1,32 @@
 class Word
-  def initialize(s)
-    @data = s.to_s
-  end
-
-  def last
-    @data[-1]
-  end
-
-  def without_last
-    @data[0..-2]
-  end
-
-  def empty?
-    @data.empty?
-  end
-
-  def to_s
-    @data
+  def self.last(s, i)
+    s[i..i]
   end
 
   def self.lcss(s1, s2)
-    memoize_result_for([s1, s2]) do
-      w1 = new(s1)
-      w2 = new(s2)
+    @memoized ||= {}
+    lcss_recursive(s1, s2, s1.size-1, s2.size-1)
+  end
 
-      if w1.empty? || w2.empty?
+  def self.lcss_recursive(s1, s2, i1, i2)
+    memoize_result_for([s1, s2, i1, i2]) do
+      if i1 == -1 || i2 == -1
         ""
-      elsif w1.last == w2.last
-        lcss(w1.without_last, w2.without_last) + w1.last
+      elsif last(s1, i1) == last(s2, i2)
+        lcss_recursive(s1, s2, i1-1, i2-1) + last(s1, i1)
       else
-        res1 = lcss(w1.without_last, w2)
-        res2 = lcss(w1, w2.without_last)
+        res1 = lcss_recursive(s1, s2, i1-1, i2)
+        res2 = lcss_recursive(s1, s2, i1, i2-1)
         res1.size >= res2.size ? res1 : res2
       end
     end
   end
 
   def self.memoize_result_for(key)
-    @memoized ||= {}
     @memoized[key] ||= yield
   end
-
 
   def self.fix_spelling(wrong, w1, w2)
     lcss(wrong, w1).size > lcss(wrong, w2).size ? w1 : w2
   end
-
 end
